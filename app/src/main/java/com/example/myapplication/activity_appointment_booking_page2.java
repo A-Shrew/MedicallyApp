1cae1198f6b7a1754ee1573ee29e1a44c218903a
Abdalla Haider <abendos203@gmail.com>
2024-12-04 17:34:20 -0800

    Added abdalla's booking appointment function

---
+++ app/src/main/java/com/example/myapplication/activity_appointment_booking_page2.java
@@ -0,0 +1,130 @@
+package com.example.myapplication;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.icu.util.Calendar;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.lang.reflect.Array;
+
+public class activity_appointment_booking_page2 extends AppCompatActivity {
+    Intent intent;
+    Bundle bundle;
+    String symptom_description;
+    int docnum;
+    Spinner doctype2, city;
+    EditText date;
+    TextView docprompt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_appointment_booking_page2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        intent = getIntent();
+        bundle = intent.getExtras();
+
+        docnum = bundle.getInt("docnum");
+        symptom_description = bundle.getString("symptom_description");
+
+        doctype2 = (Spinner) findViewById(R.id.doctype2);
+        city = (Spinner) findViewById(R.id.city);
+        date = findViewById(R.id.appointment_date);
+
+        docprompt = findViewById(R.id.docprompt);
+        String prompt_template = "Please select the type of ";
+
+
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.primary_care, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.specialist, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
+                R.array.womens, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this,
+                R.array.mental, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<CharSequence> adapter5 = ArrayAdapter.createFromResource(this,
+                R.array.other, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<CharSequence> cityarray = ArrayAdapter.createFromResource(this,
+                R.array.cities, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        city.setAdapter(cityarray);
+
+        switch(docnum) {
+            case 1: doctype2.setAdapter(adapter1);
+                    docprompt.setText(prompt_template+"primary care specialist:"); break;
+            case 2: doctype2.setAdapter(adapter2);
+                    docprompt.setText(prompt_template+" specialist specialist:"); break;
+            case 3: doctype2.setAdapter(adapter3);
+                    docprompt.setText(prompt_template+"women's/reproductive specialist:"); break;
+            case 4: doctype2.setAdapter(adapter4);
+                    docprompt.setText(prompt_template+"mental health specialist:"); break;
+            case 5: doctype2.setAdapter(adapter5);
+                    docprompt.setText(prompt_template+"other doctor"); break;
+        }
+    }
+
+    public void datepick(View view){
+        final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        String dat = dayOfMonth + "-" + (monthOfYear + 1) + "-" + year;
+                        date.setText(dat);
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+    }
+
+    public void book(View view){
+        /* Need to save them to database
+         */
+        String doctor_Type = doctype2.getSelectedItem().toString();
+        String appointment_City = city.getSelectedItem().toString();
+        String appointment_Date = date.getText().toString();
+
+
+        //save the info to next activity to display them
+        Intent intent = new Intent(this, appointment_summary.class);
+        Bundle bundle = new Bundle();
+
+        bundle.putString("symptom_description",symptom_description);
+        bundle.putString("doctor_Type",doctor_Type);
+        bundle.putString("appointment_City",appointment_City);
+        bundle.putString("appointment_Date",appointment_Date);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/appointment_booking_page1.java
@@ -0,0 +1,59 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class appointment_booking_page1 extends AppCompatActivity {
+    EditText desc;
+    RadioGroup doctortype ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_appointment_booking_page1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        desc = findViewById(R.id.Symptom_desc);
+        doctortype = findViewById(R.id.doctype);
+    }
+    public void gotopage2(View view){
+
+        int duration = Toast.LENGTH_SHORT;
+
+        int docnum = doctortype.getCheckedRadioButtonId();
+        String symptom_description = desc.getText().toString();
+
+        if (docnum == -1) {
+            Toast.makeText(this, "Please select a type of doctor", duration).show();
+            return;
+        }
+
+        if (symptom_description.isEmpty()){
+            Toast.makeText(this, "Please enter your symptoms", duration).show();
+            return;
+        }
+
+        Intent intent = new Intent(this, activity_appointment_booking_page2.class);
+        Bundle bundle = new Bundle();
+        bundle.putInt("docnum",docnum);
+        bundle.putString("symptom_description",symptom_description);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+}
