2f7e3f28a50333711b57229b8081fb73c1380ca5
Abdalla Haider <abendos203@gmail.com>
2024-12-04 17:52:32 -0800

    added home page

--- app/src/main/java/com/example/myapplication/HomePage.java
+++ app/src/main/java/com/example/myapplication/HomePage.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,15 @@ public class HomePage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void start_booking(View view){
+        Intent intent = new Intent(this, appointment_booking_page1.class);
+        startActivity(intent);
+    }
+
+    public void read_article(View view){
+        //Intent intent = new Intent(this, TODO);
+        //startActivity(intent);
     }
 }
